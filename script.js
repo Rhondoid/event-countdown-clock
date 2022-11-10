801c25be690023e8fa13c78f2278b930d2426483
Rhonda <110504360+Rhondoid@users.noreply.github.com>
2022-11-09 22:02:04 -0700

    added api, local storage startup

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 //
-const recipeURL ="https://www.themealdb.com/api/json/v1/1/list.php?i=list"
+const timeZoneURL ="https://www.timeapi.io/api/Time/current/zone?timeZone=America/Texas"
+
+
+
+{
+    "year": 2020,
+    "month": 12,
+    "day": 13,
+    "hour": 9,
+    "minute": 30,
+    "seconds": 17,
+    "milliSeconds": 0,
+    "dateTime": "2020-12-13T09:30:17",
+    "date": "13/12/2020",
+    "time": "09:30",
+    "timeZone": "America/Los_Angeles",
+    "dayOfWeek": "Sunday",
+    "dstActive": false
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// localStorage.setItem("name", "John Doe");
+// localStorage.getItem("name");
